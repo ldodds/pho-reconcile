18048621c0991b1da45374ae16e84af7b8749642
Leigh Dodds <leigh@ldodds.com>
2010-08-28 20:13:28 +0100

    Basic field tests for service metadata

--- tests/acceptance/metadata.rb
+++ tests/acceptance/metadata.rb
@@ -9,7 +9,20 @@ describe "The Reconciliation API," do
 
     it_should_behave_like "All Successful Responses"
     it_should_behave_like "All JSON Requests"
-        
+    
+    it "should have the basic required fields" do
+      query(@response, "$.name").should_not be_nil
+      query(@response, "$.identifierSpace").should_not be_nil  
+      query(@response, "$.schemaSpace").should_not be_nil
+    end
+    
+    it "should specify the resource URI as the viewer" do
+      view = query(@response, "$.view")
+      view.should_not be_nil
+      view["url"].should_not be_nil
+      view["url"].should == "{{id}}"        
+    end
+    
   end
    
 end
--- tests/acceptance/suite.spec
+++ tests/acceptance/suite.spec
@@ -1,7 +1,5 @@
 $:.unshift File.join(File.dirname(__FILE__))
-
 require "helpers/spec_helper.rb"
-
 require "shared/common.rb"
 
 #Service metadata
