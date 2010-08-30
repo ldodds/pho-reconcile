85033a4687182dc6aef0f64f6909b29bd263d032
Leigh Dodds <leigh@ldodds.com>
2010-08-30 09:25:54 +0100

    Test for type filtering

--- tests/acceptance/single-query-mode.rb
+++ tests/acceptance/single-query-mode.rb
@@ -46,5 +46,27 @@ describe "The Reconciliation API," do
       query(@response, "$.result").length.should == 1
     end
     
-  end  
+  end
+  
+  context "when performing a type query" do
+    
+    before :all do
+      test_store = "ldodds-pho-reconcile"
+      query = Hash.new
+      query["query"] = "Joe"  
+      query["type"] = "http://xmlns.com/foaf/0.1/Person"     
+      @response = server_get "/#{test_store}/reconcile?query=#{CGI::escape(query.to_json)}"
+    end
+    
+    it_should_behave_like "All Successful Responses"
+    it_should_behave_like "All JSON Requests"
+    it_should_behave_like "All Single Query Mode Requests"
+ 
+    it "should have results of only the requested type" do
+      query(@response, "$.result").each do |result|
+        result["type"][0].should == "http://xmlns.com/foaf/0.1/Person"
+      end
+    end
+        
+  end
 end
